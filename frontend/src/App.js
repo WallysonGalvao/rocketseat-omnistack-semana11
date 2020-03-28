1297540188f3944c605c69f091276be4a72d805a
Galvao Da Cunha <wgalvao@indracompany.com>
2020-03-27 22:07:36 -0300

    aplicacao frontend

---
+++ frontend/src/App.js
@@ -0,0 +1,18 @@
+import React from "react";
+import { ToastContainer } from "react-toastify";
+
+import "./global.css";
+import "react-toastify/dist/ReactToastify.css";
+
+import Routes from "./routes";
+
+function App() {
+  return (
+    <>
+      <Routes />
+      <ToastContainer autoClose={3000} />
+    </>
+  );
+}
+
+export default App;
